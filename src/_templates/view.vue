777dc648f36f195657d1fd566926ccef36f2f088
antonio-rodrigues <antonio.rodrigues@gmail.com>
2019-02-18 17:12:58 +0000

    Add vuejs template file

---
+++ src/_templates/view.vue
@@ -0,0 +1,93 @@
+<template>
+
+  <h1>Insert your HMTL here!</h1>
+
+  <button @click="onButtonClick">MY BUTTON</button>
+
+  <p>
+    STRING INTERPOLATION: {{ myLabel }}
+  </p>
+
+  <p>List example:</p>
+  <ul v-if="items && items.length">
+      <li v-for="(item, index) of items" :key="index">
+        {{ item.label }}
+      </li>
+  </ul>
+
+  <p>Add image example:</p>
+  <img src="@/assets/logo.png" />
+
+  <p>Component with props example:</p>
+  <LanguageSelector :defaultLang="lang" @onLanguageChange="onLanguageChange" />
+
+</template>
+
+<script>
+  // import here any components or libs you need
+  import { HTTP, ApiMethod } from "../../utils";
+  import LanguageSelector from "@/components/LanguageSelector";
+
+  // access VueX Redux store with getters
+  import { mapActions } from 'vuex'
+  import { mapGetters } from 'vuex'
+
+  export default {
+    name: "MyViewName",
+
+    computed: {
+      myCalculatedValue () {
+        return this.myValue * 1000;
+      },
+      hasItems: function() {
+        return !this.items.length;
+      }
+      ...mapGetters([
+        'myValue' // GET myValue from store.state = { myValue: 123 }
+      ]),
+    },
+
+    data: () => {
+      return {
+        myLabel: 'Default label, replaced on mount() lifecycle hook',
+        items: [
+          { id: 1, label: 'Item label 1' },
+          { id: 2, label: 'Item label 2' },
+          { id: 3, label: 'Item label 3' },
+        ],
+      }
+    },
+
+    // lifecycle: init
+    beforeCreate () { console.group('[LIFECYCLE]'); console.log('beforeCreate(): nothing gets called before me') },
+    created () { console.log('created(): computed will be updated') },
+
+    // lifecycle: DOM
+    beforeMount () { console.log(`beforeMount(): this.joke doesn't exist yet`) },
+    mounted () { console.log('mounted(): component did mount'); this.myLabel = 'MyLabel is now updated!!!' },
+
+    // Updating (Re-render)
+    beforeUpdate () { console.log('beforeUpdate(): before the DOM updates') },
+    updated () { console.log('updated(): after DOM updates') },
+
+    // Destruction (Teardown)
+    beforeDestroy () { console.log('beforeDestroy(): that Chuck Norris fixed look...') },
+    destroyed () { console.log('destroyed(): *PAW* You\'re gone for good!'); console.groupEnd() },
+
+    methods: {
+      onButtonClick: function(ev) {
+        console.log('YAY! Button was clicked! Event:', { ev });
+      },
+
+      onLanguageChange: function(lang) {
+        console.log('onLanguageChange:', { lang });
+      },
+    },
+
+    // add here any components you need on your template
+    components: {
+      LanguageSelector,
+    }
+  }
+</script>
+
