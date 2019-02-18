25d001e5a352382425adaa712efad3f6b8e7f008
antonio-rodrigues <antonio.rodrigues@gmail.com>
2019-02-18 16:36:22 +0000

    Refactor core layot and navigation

---
+++ src/components/RenderPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>RENDER FOR CIV VERSION: {{ civVersion }}</h1>
+</template>
+
+<script>
+export default {
+  name: "RenderPage",
+  props: {
+    civVersion: {
+      type: Number,
+      required: true
+    }
+  }
+};
+</script>
