de53470262d4abf9f45e2a20a1fea6fa6c72a91d
raen1978 <raen1978@users.noreply.github.com>
2020-10-23 16:38:51 +0100

    Update core.js
    changing to https:

--- src/utils/core.js
+++ src/utils/core.js
@@ -3,7 +3,7 @@ import axios from "axios"; // http helper lib
 export const HTTP = axios.create({
   // change URL to your base endpoint!
   baseURL:
-    "http://www.acloudfor4.com/civ6datasvc/Service1.svc/rest/civ6data.api"
+    "https://www.acloudfor4.com/civ6datasvc/Service1.svc/rest/civ6data.api"
   // headers: {
   //   Authorization: 'Bearer {token}'
   // }
