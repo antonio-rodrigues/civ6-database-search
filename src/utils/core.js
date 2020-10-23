0fb0366102a634a2ae3ce6b173de567a6fd150c1
antonio-rodrigues <antonio.rodrigues@gmail.com>
2020-10-23 16:58:07 +0100

    arod [feat] added dotenv, changed http to https

--- src/utils/core.js
+++ src/utils/core.js
@@ -1,9 +1,10 @@
 import axios from "axios"; // http helper lib
 
+require("dotenv").config();
+
 export const HTTP = axios.create({
   // change URL to your base endpoint!
-  baseURL:
-    "http://www.acloudfor4.com/civ6datasvc/Service1.svc/rest/civ6data.api"
+  baseURL: process.env.API_URL
   // headers: {
   //   Authorization: 'Bearer {token}'
   // }
